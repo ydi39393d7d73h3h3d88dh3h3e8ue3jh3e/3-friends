d72f86c4c9de03ccfbec6ce6667b70cc5e297715
Kirill Sverchkov <107706691+ksverchkov@users.noreply.github.com>
2024-09-29 01:40:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
